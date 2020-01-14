6f91d96b837a2044d1c6509c7c0eea0aa533eddd
DefbeatCZ <54858078+DefbeatCZ@users.noreply.github.com>
2020-01-14 23:51:03 +0100

    Added icons for new items (TBD)

--- media/lua/client/AdjustEmptyObjects.lua
+++ media/lua/client/AdjustEmptyObjects.lua
@@ -3,7 +3,15 @@ function Adjust(Name, Property, Value)
   Item:DoParam(Property.." = "..Value)
 end
 
-Adjust("Base.Lighter", "ReplaceOnDeplete", "DLTS.LTSDepletedLighter");
+-- Empty Versions
+Adjust("Base.Lighter", "ReplaceOnDeplete", "DLTS.LTSLighterEmpty");
 Adjust("Base.PropaneTank", "ReplaceOnDeplete", "DLTS.LTSPropaneTankEmpty");
 Adjust("Base.Glue", "ReplaceOnDeplete", "DLTS.LTSGlueEmpty");
 Adjust("Base.Woodglue", "ReplaceOnDeplete", "DLTS.LTSGlueEmpty");
+Adjust("Base.Disinfectant", "ReplaceOnDeplete", "DLTS.LTSDisinfectantEmpty");
+
+-- Jars
+Adjust("Base.Vinegar", "ReplaceOnDeplete", "Base.EmptyJar");
+Adjust("Base.Coffee2", "ReplaceOnUse", "Base.EmptyJar");
+Adjust("Base.PeanutButter", "ReplaceOnUse", "Base.EmptyJar");
+Adjust("Base.Honey", "ReplaceOnUse", "Base.EmptyJar");
