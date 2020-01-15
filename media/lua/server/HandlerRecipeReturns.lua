f8dc05b320337b8d13789e56ca5dd97a384b23f5
DefbeatCZ <54858078+DefbeatCZ@users.noreply.github.com>
2020-01-15 19:22:10 +0100

    Transformation to new file structure

--- media/lua/server/HandlerRecipeReturns.lua
+++ media/lua/server/HandlerRecipeReturns.lua
@@ -2,18 +2,6 @@ function giveDryingRack(items, result, player)
     player:getInventory():AddItem("DLTS.LTSDryingRackEmpty");
 end
 
-function giveCharcoalBurner(items, result, player)
-    player:getInventory():AddItem("DLTS.LTSWoodBurnerChar");
-end
-
-function giveEmptyBurner(items, result, player)
-    player:getInventory():AddItem("DLTS.LTSWoodBurner");
-end
-
-function giveBakingPan(items, result, player)
-    player:getInventory():AddItem("Base.BakingPan");
-end
-
 function giveRope1(items, result, player)
     player:getInventory():AddItem("Base.Rope");
 end
