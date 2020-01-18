770f4a4613d1885e108e5e7a639935d551163a63
DefbeatCZ <54858078+DefbeatCZ@users.noreply.github.com>
2020-01-18 22:43:22 +0100

    Added fermenting wine and vinegar, moonshine distillation

--- media/lua/server/HandlerRecipeReturns.lua
+++ media/lua/server/HandlerRecipeReturns.lua
@@ -2,6 +2,10 @@ function giveDryingRack(items, result, player)
     player:getInventory():AddItem("DLTS.LTSDryingRackEmpty");
 end
 
+function givePot(items, result, player)
+    player:getInventory():AddItem("Base.Pot");
+end
+
 function giveRope1(items, result, player)
     player:getInventory():AddItem("Base.Rope");
 end
