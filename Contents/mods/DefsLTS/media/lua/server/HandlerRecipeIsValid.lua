8def9fdfeb09f2e1a22fd199fa4b130e7aa291aa
DefbeatCZ <54858078+DefbeatCZ@users.noreply.github.com>
2020-04-19 01:14:00 +0200

    First Iteration of the car fix code

---
+++ Contents/mods/DefsLTS/media/lua/server/HandlerRecipeIsValid.lua
@@ -0,0 +1,3 @@
+function onTest_LTSCarMaintenance (sourceItem, result)
+	return (getSpecificPlayer(0):getVehicle() ~= nil);
+end
--- Contents/mods/DefsLTS/media/lua/server/HandlerRecipeReturns.lua
+++ Contents/mods/DefsLTS/media/lua/server/HandlerRecipeReturns.lua
@@ -6,3 +6,15 @@ function givePot(items, result, player)
     player:getInventory():AddItem("Base.Pot");
 end
 
+function giveMaintenanceTools(items, result, player)
+  player:getInventory():AddItem("Base.Scissors");
+  player:getInventory():AddItem("Base.Crowbar");
+  player:getInventory():AddItem("Base.Hammer");
+  player:getInventory():AddItem("Base.Jack");
+  player:getInventory():AddItem("Base.LugWrench");
+  player:getInventory():AddItem("Base.Screwdriver");
+  player:getInventory():AddItem("Base.TirePump");
+  player:getInventory():AddItem("Base.PipeWrench");
+  player:getInventory():AddItem("Base.Wrench");
+end
+
