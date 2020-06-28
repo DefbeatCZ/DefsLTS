2871f5e223cc79411def9c5415481c081aacd552
DefbeatCZ <54858078+DefbeatCZ@users.noreply.github.com>
2020-06-29 00:43:08 +0200

    Empty containers update

--- Contents/mods/DefsLTS/media/lua/client/AdjustEmptyObjects.lua
+++ Contents/mods/DefsLTS/media/lua/client/AdjustEmptyObjects.lua
@@ -12,9 +12,12 @@ Adjust("Base.Glue", "ReplaceOnDeplete", "DLTS.LTSChemEmpty");
 Adjust("Base.Woodglue", "ReplaceOnDeplete", "DLTS.LTSChemEmpty");
 Adjust("Base.Disinfectant", "ReplaceOnDeplete", "DLTS.LTSChemEmpty");
 Adjust("Base.CleaningLiquid2", "ReplaceOnDeplete", "DLTS.LTSChemEmpty");
+Adjust("Base.Ketchup", "ReplaceOnUse", "DLTS.LTSChemEmpty");
+Adjust("Base.Mustard", "ReplaceOnUse", "DLTS.LTSChemEmpty");
 
 -- Jars
 Adjust("Base.Vinegar", "ReplaceOnDeplete", "Base.EmptyJar");
 Adjust("Base.Coffee2", "ReplaceOnUse", "Base.EmptyJar");
 Adjust("Base.PeanutButter", "ReplaceOnUse", "Base.EmptyJar");
 Adjust("Base.Honey", "ReplaceOnUse", "Base.EmptyJar");
+Adjust("Base.Marinara", "ReplaceOnUse", "Base.EmptyJar");
