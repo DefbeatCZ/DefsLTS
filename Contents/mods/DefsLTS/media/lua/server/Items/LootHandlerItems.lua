0fd8010e0187ccb72dc1dab80477b585d93e2b7a
DefbeatCZ <54858078+DefbeatCZ@users.noreply.github.com>
2021-08-15 21:21:06 +0200

    Update LootHandlerItems.lua

--- Contents/mods/DefsLTS/media/lua/server/Items/LootHandlerItems.lua
+++ Contents/mods/DefsLTS/media/lua/server/Items/LootHandlerItems.lua
@@ -1,96 +1,71 @@
-require 'Items/Distributions'
+require 'Items/ProceduralDistributions'
 
-function registerAsLoot(item, chance, sub1, sub2)
-  table.insert(SuburbsDistributions[sub1][sub2].items, item);
-  table.insert(SuburbsDistributions[sub1][sub2].items, chance);
+function registerAsLoot(item, chance, allocation)
+  table.insert(ProceduralDistributions.list[allocation].items, item);
+  table.insert(ProceduralDistributions.list[allocation].items, chance);
 end
 
 local iReg = "UseMeLater";
 
 -- Glass Jar
 iReg = "EmptyJar"
-registerAsLoot(iReg, 2.0, "grocerystorage", "bin");
-registerAsLoot(iReg, 2.0, "spiffoskitchen", "counter");
-registerAsLoot(iReg, 2.0, "bar", "bin");
-registerAsLoot(iReg, 0.7, "all", "bin");
+registerAsLoot(iReg, 2.0, "KitchenBottles");
+registerAsLoot(iReg, 2.0, "KitchenCannedFood");
+registerAsLoot(iReg, 2.0, "KitchenRandom");
+registerAsLoot(iReg, 2.0, "BinBar");
+registerAsLoot(iReg, 2.0, "BinGeneric");
 
 -- Box of Empty Jars
 iReg = "BoxOfJars"
-registerAsLoot(iReg, 0.2, "garagestorage", "other");
-registerAsLoot(iReg, 0.1, "garage", "metal_shelves");
-registerAsLoot(iReg, 0.1, "storageunit", "all");
+registerAsLoot(iReg, 1.0, "GarageTools");
+registerAsLoot(iReg, 1.0, "CrateTools");
 
 -- Lighter Fluid
 iReg = "DLTS.LTSLighterFluid";
-registerAsLoot(iReg, 1.5, "zippeestore", "counter");
-registerAsLoot(iReg, 4.0, "zippeestore", "crate");
-registerAsLoot(iReg, 2.0, "fossoil", "crate");
-registerAsLoot(iReg, 0.2, "shed", "other");
-registerAsLoot(iReg, 0.2, "generalstorestorage", "other");
-registerAsLoot(iReg, 2.8, "gasstore", "shelves");
-registerAsLoot(iReg, 0.3, "all", "metal_shelves");
+registerAsLoot(iReg, 2.0, "BarCounterMisc");
+registerAsLoot(iReg, 4.0, "StoreCounterTobacco");
+registerAsLoot(iReg, 3.0, "CampingStoreGear");
+registerAsLoot(iReg, 5.0, "CrateCamping");
+registerAsLoot(iReg, 1.0, "KitchenRandom");
 
 -- Empty Water Can 
 iReg = "DLTS.LTSWaterCanEmpty";
-registerAsLoot(iReg, 1.5, "loggingfactory", "crate");
-registerAsLoot(iReg, 0.5, "shed", "other");
-registerAsLoot(iReg, 0.5, "generalstorestorage", "other");
-registerAsLoot(iReg, 0.5, "generalstorestorage", "other");
-registerAsLoot(iReg, 0.3, "storageunit", "all");
-registerAsLoot(iReg, 0.1, "all", "metal_shelves");
+
+registerAsLoot(iReg, 1.0, "GarageTools");
+registerAsLoot(iReg, 1.0, "CrateTools");
+registerAsLoot(iReg, 6.0, "CampingStoreGear");
+registerAsLoot(iReg, 2.0, "CrateCamping");
+registerAsLoot(iReg, 1.0, "KitchenRandom");
 
 -- Water Can with Water
 iReg = "DLTS.LTSWaterCanFilled"
-registerAsLoot(iReg, 1.3, "zippeestore", "counter");
-registerAsLoot(iReg, 0.5, "zippeestore", "crate");
-registerAsLoot(iReg, 0.5, "fossoil", "crate");
-registerAsLoot(iReg, 0.5, "gasstore", "shelves");
-registerAsLoot(iReg, 0.5, "gasstorage", "metal_shelves");
+registerAsLoot(iReg, 6.0, "CrateCamping");
+registerAsLoot(iReg, 1.0, "KitchenRandom");
 
 -- Awesome DYI - Woodwork
 iReg = "DLTS.LTSWorkbookWood";
-registerAsLoot(iReg, 0.3, "poststorage", "all");
-registerAsLoot(iReg, 0.2, "toolstore", "shelves");
-registerAsLoot(iReg, 0.2, "toolstore", "counter");
-registerAsLoot(iReg, 0.5, "bookstore", "other");
-registerAsLoot(iReg, 0.3, "garage", "metal_shelves");
-registerAsLoot(iReg, 0.3, "all", "shelves");
-registerAsLoot(iReg, 0.2, "zippeestore", "shelvesmag");
-registerAsLoot(iReg, 0.2, "cornerstore", "shelvesmag");
+registerAsLoot(iReg, 0.6, "BookstoreBooks");
+registerAsLoot(iReg, 0.3, "PostOfficeBooks");
+registerAsLoot(iReg, 0.5, "LibraryBooks");
+registerAsLoot(iReg, 0.3, "LivingRoomShelf");
 
 -- Awesome DYI - Electricity
 iReg = "DLTS.LTSWorkbookElectricity";
-registerAsLoot(iReg, 0.3, "electronicsstore", "counter");
-registerAsLoot(iReg, 0.3, "poststorage", "all");
-registerAsLoot(iReg, 0.2, "toolstore", "shelves");
-registerAsLoot(iReg, 0.2, "toolstore", "counter");
-registerAsLoot(iReg, 0.5, "bookstore", "other");
-registerAsLoot(iReg, 0.3, "garage", "metal_shelves");
-registerAsLoot(iReg, 0.3, "all", "shelves");
-registerAsLoot(iReg, 0.2, "zippeestore", "shelvesmag");
-registerAsLoot(iReg, 0.2, "cornerstore", "shelvesmag");
+registerAsLoot(iReg, 0.6, "BookstoreBooks");
+registerAsLoot(iReg, 0.3, "PostOfficeBooks");
+registerAsLoot(iReg, 0.5, "LibraryBooks");
+registerAsLoot(iReg, 0.3, "LivingRoomShelf");
 
--- Awesome DYI - Welding
+-- Awesome DYI - Welding-
 iReg = "DLTS.LTSWorkbookWelding";
-registerAsLoot(iReg, 0.3, "mechanic", "wardrobe");
-registerAsLoot(iReg, 0.3, "mechanic", "metal_shelves");
-registerAsLoot(iReg, 0.3, "poststorage", "all");
-registerAsLoot(iReg, 0.2, "toolstore", "shelves");
-registerAsLoot(iReg, 0.2, "toolstore", "counter");
-registerAsLoot(iReg, 0.5, "bookstore", "other");
-registerAsLoot(iReg, 0.3, "garage", "metal_shelves");
-registerAsLoot(iReg, 0.3, "all", "shelves");
-registerAsLoot(iReg, 0.2, "zippeestore", "shelvesmag");
-registerAsLoot(iReg, 0.2, "cornerstore", "shelvesmag");
+registerAsLoot(iReg, 0.6, "BookstoreBooks");
+registerAsLoot(iReg, 0.3, "PostOfficeBooks");
+registerAsLoot(iReg, 0.5, "LibraryBooks");
+registerAsLoot(iReg, 0.3, "LivingRoomShelf");
 
 -- Awesome DYI - Tailoring
 iReg = "DLTS.LTSWorkbookTailoring";
-registerAsLoot(iReg, 0.3, "sewingstore", "counter");
-registerAsLoot(iReg, 0.3, "poststorage", "all");
-registerAsLoot(iReg, 0.2, "toolstore", "shelves");
-registerAsLoot(iReg, 0.2, "toolstore", "counter");
-registerAsLoot(iReg, 0.5, "bookstore", "other");
-registerAsLoot(iReg, 0.3, "garage", "metal_shelves");
-registerAsLoot(iReg, 0.3, "all", "shelves");
-registerAsLoot(iReg, 0.2, "zippeestore", "shelvesmag");
-registerAsLoot(iReg, 0.2, "cornerstore", "shelvesmag");
+registerAsLoot(iReg, 0.6, "BookstoreBooks");
+registerAsLoot(iReg, 0.3, "PostOfficeBooks");
+registerAsLoot(iReg, 0.5, "LibraryBooks");
+registerAsLoot(iReg, 0.3, "LivingRoomShelf");
